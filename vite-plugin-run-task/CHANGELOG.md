5fa356002e92a46d17e7414a33f977c7317eb72a
Vijay Aswadhati <vijay@m5nv.com>
2025-03-10 15:38:10 -0700

    vite-plugin-run-task: chore: fix links and doc typos

--- vite-plugin-run-task/CHANGELOG.md
+++ vite-plugin-run-task/CHANGELOG.md
@@ -1,5 +1,9 @@
 # [Changelog](https://github.com/million-views/packages/commits/main/vite-plugin-run-task)
 
+## v1.0.2 - 10MAR2025
+
+- chore: fix links and doc typos
+
 ## v1.0.1 - 07MAR2025
 
 - doc: update README and add CHANGELOG.
--- vite-plugin-run-task/src/index.js
+++ vite-plugin-run-task/src/index.js
@@ -1,39 +1,38 @@
 /**
  * vite-plugin-run-task: invoke user defined task on file/folder changes.
- * 
+ *
  * This plugin allows users to tap into Vite plugin's `configResolved`
- * and `handHotUpdate` hooks and execute a task to be run when one or
+ * and `handleHotUpdate` hooks and execute a task to be run when one or
  * more files/folders matching a specification is saved.
- * 
+ *
  * User defined task can optionally return an event payload that will be
  * sent to clients regarding the nature of the change; the task should
- * return  * false if it has no updates or is not interested in hmr.
- * 
+ * return  *false* if it has no updates or is not interested in hmr.
  */
 
-import path from 'node:path';
-import { minimatch } from 'minimatch';
+import path from "node:path";
+import { minimatch } from "minimatch";
 
 const match = (watch, file, root) => {
   return watch.some((pattern) => {
     pattern = path.resolve(root, pattern).replaceAll("\\", "/");
     return minimatch(file, pattern);
   });
-}
+};
 
-export default function ({watch, task, }) {
+export default function ({ watch, task }) {
   if (!Array.isArray(watch)) {
     throw TypeError(`watch expects an Array of strings; ${watch} is not that!`);
   }
-  
+
   // Minimatch requires forward slashes in glob expressions
   watch = watch.map((path) => path.replaceAll("\\", "/"));
 
   return {
-    name: 'vite-plugin-run-task',
+    name: "vite-plugin-run-task",
 
     configResolved(resolvedConfig) {
-      task(resolvedConfig, 'configResolved');
+      task(resolvedConfig, "configResolved");
     },
 
     handleHotUpdate(context) {
@@ -41,10 +40,10 @@ export default function ({watch, task, }) {
       const { root } = server.config;
       let modules = context.modules;
       if (match(watch, file, root)) {
-        const followup = task(context, 'hotUpdate');
+        const followup = task(context, "hotUpdate");
 
         // followup must have 'type' key to qualify as payload for `send`
-        if (followup && Object.hasOwn(followup, 'type')) {
+        if (followup && Object.hasOwn(followup, "type")) {
           server.ws.send(followup);
         }
 
@@ -53,6 +52,6 @@ export default function ({watch, task, }) {
       }
 
       return modules;
-    }
-  }
+    },
+  };
 }
